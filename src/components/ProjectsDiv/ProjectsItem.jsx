4e4d24fd040fd84264fc5871590a5785f0c15a80
Hugosomers <hugosomersla@gmail.com>
2021-08-09 18:58:13 -0300

    Add arquivo que contem os dados dos projetos

--- src/components/ProjectsDiv/ProjectsItem.jsx
+++ src/components/ProjectsDiv/ProjectsItem.jsx
@@ -1,43 +1,36 @@
 import React from 'react';
 import projectsStyles from '../../css/projects.module.css';
+import { Link } from 'react-router-dom';
 import { AiOutlineGithub } from 'react-icons/ai';
+import projectsData from '../../projectsData';
 
 export default function ProjectsItem() {
-  const array = ['1','1','1','1','1','1','1','1','1'];
   return (
     <>
-    {array.map((item, index) => (
-      <div
-        key={index}
-        className={projectsStyles.projectItemContainer}
-      >
-        <img alt="ProjectItem"/>
-        <div
-          className={projectsStyles.projectItemDescription}
-        >
-          <h1
-            className={projectsStyles.projectItemTitle}
-          >
-            Projeto {index}
-          </h1>
-          <p
-            className={projectsStyles.projectItemText}
-          >
-            Descrição do projeto Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit distinctio quia quibusdam vero doloremque repellendus, quas, provident magni eius deleniti ut delectus ducimus quos corrupti numquam ipsa? Eos, ad delectus. {index}
-          </p>
-          <div 
-            className={projectsStyles.projectItemButtonsContainer}
-          >
-            <button
-              className={projectsStyles.projectItemButton}
-            >
-              Acessar Projeto
-            </button>
-            <AiOutlineGithub  size={40} color={'#AD5C51'} className={projectsStyles.projectsIcons}/>
+      {projectsData.map((item, index) => (
+        <div key={index} className={projectsStyles.projectItemContainer}>
+          <img alt="ProjectItem" />
+          <div className={projectsStyles.projectItemDescription}>
+            <h1 className={projectsStyles.projectItemTitle}>{item.name}</h1>
+            <p className={projectsStyles.projectItemText}>{item.description}</p>
+            <div className={projectsStyles.projectItemButtonsContainer}>
+              <button className={projectsStyles.projectItemButton}>
+                Acessar Projeto
+              </button>
+              <Link
+                to={{pathname: item.gitHubLink}}
+                target='_blank'
+              >
+                <AiOutlineGithub
+                  size={40}
+                  color={'#AD5C51'}
+                  className={projectsStyles.projectsIcons}
+                />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
+      ))}
     </>
-  )
+  );
 }
---
+++ src/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    name: 'Facebook Login Page',
+    image: '',
+    description: 'Projeto desenvolvido com a finalidade de praticar Redux e suas funcionalidades, entender a ideia de autenticação(feita somente por front-end) e aplicar conhecimentos em CSS para chegar a resultados parecidos com o site oficial do Facebook.',
+    gitHubLink: 'https://github.com/Hugosomers/facebookProject'
+  },
+  {
+    name: 'Recipes App',
+    image: '',
+    description: 'Projeto em grupo na Trybe, o objetivo era desenvolver um aplicativo de consulta das mais variadas receitas de comidas e drinks disponíveis nas APIs passadas pela Trybe.',
+    gitHubLink: 'https://github.com/tryber/sd-011-project-recipes-app/pull/35',
+  },
+];
+
+export default projectsData;
