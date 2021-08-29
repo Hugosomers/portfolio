2a07fa7f0405e5722faccd55c2432815a7590e84
Hugosomers <hugosomersla@gmail.com>
2021-08-29 11:20:22 -0300

    Clean binemonApp Power Feature

--- src/components/BinemonApp/PowerfulIdentifier.jsx
+++ src/components/BinemonApp/PowerfulIdentifier.jsx
@@ -9,9 +9,9 @@ export default function PowerfulIdentifier({monClass, monSkills, monBreeds}) {
 
   // Combos do Unicórnio Suporte
   const cU1 = monSkills[0] === 'The_General' && monSkills[1] === 'The_Strongest_Sidekick';
-  const cU2 = monSkills[0] === 'The_General' && monSkills[1] === 'Blessing_Of_Life';
+  const cU2 = monSkills[0] === 'The_General' && monSkills[1] === 'Blessing_of_Life';
   const cU3 = monSkills[0] === 'Ancient_Wisdom' && monSkills[1] === 'The_Strongest_Sidekick';
-  const cU4 = monSkills[0] === 'Ancient_Wisdom' && monSkills[1] === 'Blessing_Of_Life';
+  const cU4 = monSkills[0] === 'Ancient_Wisdom' && monSkills[1] === 'Blessing_of_Life';
 
   // Combos do Titan Tanker
   const cT1 = monSkills[0] === 'Fortify' && monSkills[1] === 'Earthquake_Strike';
@@ -32,103 +32,65 @@ export default function PowerfulIdentifier({monClass, monSkills, monBreeds}) {
   const cDF1 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Berserker_Mode';
   const cDF2 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Rapid_Severence';
 
-    // Combos do Dog Archer
-    const cDA1 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Projectile_Barrage';
-    const cDA2 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Deathblow';
-    const cDA3 = monSkills[0] === 'Adapter' && monSkills[1] === 'Projectile_Barrage';
-    const cDA4 = monSkills[0] === 'Adapter' && monSkills[1] === 'Deathblow';
+  // Combos do Dog Archer
+  const cDA1 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Projectile_Barrage';
+  const cDA2 = monSkills[0] === 'Fighter_Spirit' && monSkills[1] === 'Deathblow';
+  const cDA3 = monSkills[0] === 'Adapter' && monSkills[1] === 'Projectile_Barrage';
+  const cDA4 = monSkills[0] === 'Adapter' && monSkills[1] === 'Deathblow';
 
-    // Combos do Cat Fighter
-    const cCF1 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Berserker_Mode';
-    const cCF2 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Rapid_Severence';
-    const cCF3 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Berserker_Mode';
-    const cCF4 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Rapid_Severence';
+  // Combos do Cat Fighter
+  const cCF1 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Berserker_Mode';
+  const cCF2 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Rapid_Severence';
+  const cCF3 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Berserker_Mode';
+  const cCF4 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Rapid_Severence';
 
-    // Combos do Cat Archer
-    const cCA1 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Projectile_Barrage';
-    const cCA2 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Deathblow';
-    const cCA3 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Projectile_Barrage';
-    const cCA4 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Deathblow';
+  // Combos do Cat Archer
+  const cCA1 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Projectile_Barrage';
+  const cCA2 = monSkills[0] === 'Battle_Blood' && monSkills[1] === 'Deathblow';
+  const cCA3 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Projectile_Barrage';
+  const cCA4 = monSkills[0] === 'Swift_Movements' && monSkills[1] === 'Deathblow';
 
-    const unicornSetup = () => {
-      if (monBreeds === 'Unicorn' && monClass === 'Support') {
-        if (cU1 || cU2 || cU3 || cU4) {
-          dispatch(setPower('strong'));
-          
-        } else {
-          dispatch(setPower('weak'));
-          
-        }
-      } else {
-        dispatch(setPower('weak'));
-      }
+  const unicornSetup = () => {
+    if (monClass === 'Support' && (cU1 || cU2 || cU3 || cU4)) {
+      dispatch(setPower('strong'));
+    } else {
+      dispatch(setPower('weak'));
     }
+  }
 
-    const titanSetup = () => {
-      if ( monClass === 'Tanker') {
-        if (cT1 || cT2) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else if (monClass === 'Fighter') {
-        if (cTF1 || cTF2) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      }  else if (monClass === 'Mage') {
-        if (cTM1 || cTM2) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else {
-        dispatch(setPower('weak'));
-      }
+  const titanSetup = () => {
+    if (monClass === 'Tanker' && (cT1 || cT2)) {
+      dispatch(setPower('strong'));
+    } else if (monClass === 'Fighter' && (cTF1 || cTF2)) {
+      dispatch(setPower('strong'));
+    } else if (monClass === 'Mage' && (cTM1 || cTM2)) {
+      dispatch(setPower('strong'));
+    } else {
+      dispatch(setPower('weak'));
     }
+  }
 
-    const dogSetup = () => {
-      if (monClass === 'Tanker') {
-        if (cDT1) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else if (monClass === 'Fighter') {
-        if (cDF1 || cDF2) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else if (monClass === 'Archer') {
-        if (cDA1 || cDA2 || cDA3 || cDA4) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else {
-        dispatch(setPower('weak'));
-      }
+  const dogSetup = () => {
+    if (monClass === 'Tanker' && cDT1) {
+      dispatch(setPower('strong'));
+    } else if (monClass === 'Fighter' && (cDF1 || cDF2)) {
+      dispatch(setPower('strong'));
+    } else if (monClass === 'Archer' && (cDA1 || cDA2 || cDA3 || cDA4)) {
+      dispatch(setPower('strong'));
+    } else {
+      dispatch(setPower('weak'));
     }
+  }
 
-    const catSetup = () => {
-      if (monClass === 'Fighter') {
-        if (cCF1 || cCF2 || cCF3 || cCF4) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else if (monClass === 'Archer') {
-        if (cCA1 || cCA2 || cCA3 || cCA4) {
-          dispatch(setPower('strong'));
-        } else {
-          dispatch(setPower('weak'));
-        }
-      } else {
-        dispatch(setPower('weak'));
-      }
+  const catSetup = () => {
+    if (monClass === 'Fighter' && (cCF1 || cCF2 || cCF3 || cCF4)) {
+      dispatch(setPower('strong'));
+    } else if (monClass === 'Archer' && (cCA1 || cCA2 || cCA3 || cCA4)) {
+      dispatch(setPower('strong'));
+    } else {
+      dispatch(setPower('weak'));
     }
+  }
 
   useEffect(() => {
     if (monBreeds === 'Unicorn') {
